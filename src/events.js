b6fae147b75f09c6974ed1354536621470b42ec3
Destry Ullrich <wullrich@cnm.edu>
2024-08-14 01:13:47 -0600

    created single section animation effects

--- src/events.js
+++ src/events.js
@@ -1,5 +1,21 @@
-const whoInnerHtml = document.getElementById("whoSection");
 
-function whoTransition(){
-    console.log(whoInnerHtml)
+
+function whoEffect(){
+    document.getElementById('whoClick').classList.add("fade-out");
+    document.getElementById('whoTitle').classList.add("fade-title");
+
+    document.getElementById('whoText0').classList.add("fade-0");
+    document.getElementById('whoText1').classList.add("fade-1");
+    document.getElementById('whoText2').classList.add("fade-2");
+    document.getElementById('whoText3').classList.add("fade-3");
+    document.getElementById('whoText4').classList.add("fade-4");
+    document.getElementById('whoText5').classList.add("fade-5");
+    document.getElementById('whoText6').classList.add("fade-6");
+    document.getElementById('whoText7').classList.add("fade-7");
+    document.getElementById('whoText8').classList.add("fade-8");
+
+    document.getElementById('whoExample').classList.add("fade-example");
+
+    // whoInnerHtml.classList.add("fade-0");
+    // document.getElementById("whoSection").classList.remove("fade-0");
 }
