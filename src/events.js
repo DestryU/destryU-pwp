fff311ae393a26a87799307d83c00e9505d523d0
Destry Ullrich <wullrich@cnm.edu>
2024-08-27 05:32:54 -0600

    Final draft for project turn in.

--- src/events.js
+++ src/events.js
@@ -159,18 +159,6 @@ function whyReset() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 function howSection() {
     const targetSection = document.getElementById('howSection')
 
@@ -207,5 +195,55 @@ function howSection() {
             <img src="https://placehold.co/300x300" alt="">
         </button>
     </div>
+    `
+}
+
+
+
+function sillyMessages() {
+    let i = Math.floor(Math.random() * 11)
+    console.log(i)
+    let messages = [
+        "Stop",
+        "Stoppit!",
+        "Too slow",
+        "Nuh-uh",
+        "You missed",
+        "Don't",
+        "Nope",
+        "Don't try",
+        "Told you",
+        "No more",
+        ":P"
+    ]
+    return messages[i]
+}
+
+
+
+
+
+function sillyOffset() {
+    const targetSection = document.getElementById('keepAway')
+
+    let i = Math.floor(Math.random() * 11)
+
+    let temp = [
+        "translate-x-[185px] translate-y-[22px]",
+        "translate-x-[4px] translate-y-[132px]",
+        "translate-x-[153px] translate-y-[160px]",
+        "translate-x-[101px] translate-y-[209px]",
+        "translate-x-[110px] translate-y-[191px]",
+        "translate-x-[204px] translate-y-[80px]",
+        "translate-x-[19px] translate-y-[4px]",
+        "translate-x-[36px] translate-y-[37px]",
+        "translate-x-[96px] translate-y-[150px]",
+        "translate-x-[170px] translate-y-[213px]",
+        "translate-x-[204px] translate-y-[116px]",
+        "translate-x-[18px] translate-y-[36px]"
+        ]
+
+    targetSection.innerHTML = `
+        <button onmouseenter="sillyOffset()" onclick="sillyOffset()" class="bg-neutral-800 text-neutral-300 max-w-fit h-fit p-3 rounded-2xl z-[9999] ${temp[i]}">${sillyMessages()}</button>
     `
 }
